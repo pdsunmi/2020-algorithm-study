cf3108f03a53a24893c5a2d8282513c69edff30c
Yang Sun Mi <pdsunmii@gmail.com>
2020-05-29 23:00:13 +0900

    Update 11404.cpp

--- floyd-warshall/11404.cpp
+++ floyd-warshall/11404.cpp
@@ -2,13 +2,13 @@
 #include <vector>
 using namespace std;
 
+#define INF INT32_MAX
+
 int main()
 {
 	int i, j, k, n, m, a, b, c;
-	int inf = 1000000000;
-
 	cin >> n >> m;	
-	vector<vector<int>> v(n, vector<int>(n, inf));
+	vector<vector<int>> v(n, vector<int>(n, INF));
 
 	// initialize
 	for (i = 0; i < n; i++)
@@ -21,7 +21,7 @@ int main()
 		if (v[a-1][b-1] > c)
 			v[a-1][b-1] = c;
 	}
-	
+
 	//floyd-warshall
 	for (k = 0; k < n; k++)
 	{
@@ -29,7 +29,9 @@ int main()
 		{
 			for (j = 0; j < n; j++)
 			{
-				if (i == j)
+				if (i == j || k == i || k == j)
+					continue;
+				if (v[i][k] == INF || v[k][j] == INF)
 					continue;
 				if (v[i][k] + v[k][j] < v[i][j])
 					v[i][j] = v[i][k]+v[k][j];
@@ -42,7 +44,7 @@ int main()
 	{
 		for (j = 0; j < n; j++)
 		{
-			if (v[i][j] == inf)
+			if (v[i][j] == INF)
 				cout << "0 ";
 			else
 				cout << v[i][j] << " ";
